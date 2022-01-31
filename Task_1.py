997ccfcf656bdec6684f350b3c614434953a41be
anurzh <anuarzhaksylyk@gmail.com>
2022-01-31 22:09:42 +0300

    Task 1

---
+++ Task_1.py
@@ -0,0 +1,7 @@
+a = 10
+b = 15
+c = int(input("Input NUMBER:"))
+d = int(input("Input one more NUMBER:"))
+e = input("Type symbol:")
+f = input("Type another symbol:")
+print(a, b, "is default \n Your numbers and symbols %d,%d,%s,%s" % (c, d, e, f))
