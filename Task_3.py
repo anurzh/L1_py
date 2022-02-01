6ec898ed2319a09bf46ccffb17e46dd7cb0d51f7
anurzh <anuarzhaksylyk@gmail.com>
2022-02-01 23:15:49 +0600

    Task 3

--- Task_3.py
+++ Task_3.py
@@ -1,4 +1,7 @@
 n = int(input("Input number 'n':"))
+while n < 0:
+    print("Please enter number greater than 0")
+    n = int(input("Input number 'n':"))
 n2 = int(str(n) + str(n))
 n3 = int(str(n) + str(n) + str(n))
 total = n + n2 + n3
