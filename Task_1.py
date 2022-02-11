ed88814f1ff2d91debd67a9a83b9bcb35ea9dfe9
anurzh <anuarzhaksylyk@gmail.com>
2022-02-11 21:28:08 +0600

    Task 5

--- Task_1.py
+++ Task_1.py
@@ -1,3 +1,9 @@
-all_list = [13, 'word', 3.3, True, 3+2j, None, list('word'), tuple('word'), set('word')]
-for i in all_list:
-    print(f"{i} - {type(i)}")
+def divide(a, b):
+    try:
+        num_div = int(a) / int(b)
+    except ValueError:
+        return "Erorr"
+    return (num_div)
+
+
+print(divide(input("Enter number:"), input("Enter one more number:")))
