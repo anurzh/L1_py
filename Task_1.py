c229ca2cf7ed2ea47f7d1203e8556b3d7dfe1542
anurzh <anuarzhaksylyk@gmail.com>
2022-02-18 18:07:56 +0600

    Task 1

--- Task_1.py
+++
@@ -1,9 +0,0 @@
-def divide(a, b):
-    try:
-        num_div = int(a) / int(b)
-    except ValueError:
-        return "Error"
-    return num_div
-
-
-print(divide(input("Enter number:"), input("Enter one more number:")))
